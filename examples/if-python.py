488850e36a638e8d2eca36e351bf58da59737962
Purwanto <purwanto1337@gmail.com>
2017-10-19 18:00:22 +0700

    Create if-python.py

---
+++ examples/if-python.py
@@ -0,0 +1,11 @@
+#Kondisi if adalah kondisi yang akan dieksekusi oleh program jika bernilai benar atau TRUE
+
+nilai = 9
+
+#jika kondisi benar/TRUE maka program akan mengeksekusi perintah dibawahnya
+if(nilai > 7):
+    print("Selamat Anda Lulus")
+
+#jika kondisi salah/FALSE maka program tidak akan mengeksekusi perintah dibawahnya
+if(nilai > 10):
+    print("Selamat Anda Lulus")
