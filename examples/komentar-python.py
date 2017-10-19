1242a02d86f0a2b5c513907a31dad5a594ada374
Purwanto <purwanto1337@gmail.com>
2017-10-19 17:56:01 +0700

    Create komentar-python.py

---
+++ examples/komentar-python.py
@@ -0,0 +1,19 @@
+#Contoh komentar
+# Ini adalah komentar pada pemrograman python
+
+#komentar pada python hanya bisa digunakan
+#untuk
+#satu
+#baris
+
+print("Hello World") #ini juga komentar
+
+#print("Welcome")
+
+# komentar bisa berisi spesial karakter !@#$%^&*(),./;'[]\
+
+#mencetak string
+print("Kelastutorial")
+
+#mencetak angka/integer
+print(123)
